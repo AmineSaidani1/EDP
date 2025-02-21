6ed12b1109ded3e8156eac8d9a20936a84bdc985
Amine <amine.saidani.101@gmail.com>
2025-02-21 11:26:51 +0100

    crear los tests v1

--- src/Dict.java
+++ src/Dict.java
@@ -1,7 +1,33 @@
-public class Dict {
-public String Dict() {
-    String s = ("Hola Diccionario");
-    return s;
-}
+/**
+ * TODO
+ * crear y desarrollar los metodos:
+ * void put(K key, V value)
+ * V get(K key)
+ * void remove(K key)
+ * boolean containsKey(K key)
+ * int size()
+ * boolean isEmpty()
+ */
+
+import java.util.*;
+
+public class Dict<K, V> {
+    private final HashMap<K, V> map;
+
+    public Dict() {
+        this.map = new HashMap<>();
+    }
+    public void put(K key, V value) {}
+
+    public V get(K key) {
+    }
+
+    public V remove(K key) {}
+
+    public boolean containsKey(K key) {}
+
+    public int size() {}
+
+    public boolean isEmpty() {}
 
 }
